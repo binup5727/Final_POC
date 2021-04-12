158daa5cee733c38b83196673def4eecb0cc9671
home <bmathai2014@fau.edu>
2021-04-12 16:37:58 -0400

    second

---
+++ app/src/main/java/com/example/final_poc/favorite_fragment.java
@@ -0,0 +1,27 @@
+package com.example.final_poc;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class favorite_fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.nav_favorite, container, false);
+
+        TextView name = view.findViewById(R.id.stock_name);
+        name.setText("fuck this is here finally");
+
+
+
+        return view;
+    }
+
+}
