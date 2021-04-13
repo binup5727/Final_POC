567e7959d1dfdf8f1e8cbffb4bfff9880a92aca2
binup5727 <73069106+binup5727@users.noreply.github.com>
2021-04-12 21:27:27 -0400

    almost final

---
+++ app/src/main/java/com/example/final_poc/home_fragment.java
@@ -0,0 +1,34 @@
+package com.example.final_poc;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+
+public class home_fragment extends Fragment {
+    SignInButton signInButton;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = (View)inflater.inflate(R.layout.nav_home, container, false);
+
+
+
+
+
+        return view;
+    }
+
+}
